6795cbd81161669ce5bdd108751f5088e6c9e43a
Kisan Thapa <kisanthapa33@gmail.com>
2022-03-20 14:11:32 -0400

    Homework 8 added

---
+++ hw08/src/edu/umb/cs681/hw08/PrimeFactorizer.java
@@ -0,0 +1,89 @@
+package edu.umb.cs681.hw08;
+
+import java.util.LinkedList;
+
+// Generates prime factors of a given number (dividend)
+// Prime factors are generated in the range of 2 and dividend
+// from:	lower bound of the range (2)
+// to:		upper bound of the range (dividend)
+
+public class PrimeFactorizer {
+    protected long dividend, from, to;
+    protected LinkedList<Long> factors = new LinkedList<>();
+
+    public PrimeFactorizer(long dividend) {
+        if (dividend >= 2) {
+            this.dividend = dividend;
+            this.from = 2;
+            this.to = dividend;
+        } else {
+            throw new RuntimeException("dividend must be >= 2. dividend==" + dividend);
+        }
+    }
+
+    public long getDividend() {
+        return dividend;
+    }
+
+    public long getFrom() {
+        return from;
+    }
+
+    public long getTo() {
+        return to;
+    }
+
+    public LinkedList<Long> getPrimeFactors() {
+        return factors;
+    }
+
+    public void generatePrimeFactors() {
+        long divisor = 2;
+        while (dividend != 1 && divisor <= to) {
+            if (dividend % divisor == 0) {
+                factors.add(divisor);
+                dividend /= divisor;
+            } else {
+                if (divisor == 2) divisor++;
+                else divisor += 2;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Prime factors of 6: ");
+        PrimeFactorizer fac = new PrimeFactorizer(6);
+        fac.generatePrimeFactors();
+        System.out.println(fac.getPrimeFactors());
+
+        System.out.print("Prime factors of 17: ");
+        fac = new PrimeFactorizer(17);
+        fac.generatePrimeFactors();
+        System.out.println(fac.getPrimeFactors());
+
+        System.out.print("Prime factors of 131: ");
+        fac = new PrimeFactorizer(131);
+        fac.generatePrimeFactors();
+        System.out.println(fac.getPrimeFactors());
+
+        System.out.print("Prime factors of 84: ");
+        fac = new PrimeFactorizer(84);
+        fac.generatePrimeFactors();
+        System.out.println(fac.getPrimeFactors());
+
+        System.out.print("Prime factors of 125: ");
+        fac = new PrimeFactorizer(125);
+        fac.generatePrimeFactors();
+        System.out.println(fac.getPrimeFactors());
+
+        System.out.print("Prime factors of 2489: ");
+        fac = new PrimeFactorizer(2489);
+        fac.generatePrimeFactors();
+        System.out.println(fac.getPrimeFactors());
+
+        System.out.print("Prime factors of 8633: ");
+        fac = new PrimeFactorizer(8633);
+        fac.generatePrimeFactors();
+        System.out.println(fac.getPrimeFactors());
+    }
+}
---
+++ hw08/src/edu/umb/cs681/hw08/RunnableCancellableInterruptiblePrimeFactorizer.java
@@ -0,0 +1,122 @@
+package edu.umb.cs681.hw08;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RunnableCancellableInterruptiblePrimeFactorizer extends RunnableCancellablePrimeFactorizer {
+    private boolean done = false;
+    private ReentrantLock lock = new ReentrantLock();
+
+    public RunnableCancellableInterruptiblePrimeFactorizer(long dividend, long from, long to) {
+        super(dividend, from, to);
+    }
+
+    public void setDone() {
+        lock.lock();
+        try {
+            done = false;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void generatePrimeFactors() {
+        long divisor = from;
+        while (dividend != 1 && divisor <= to) {
+            lock.lock();
+            try {
+                if (done) {
+                    System.out.println("Factors generation stopped");
+                    this.factors.clear();
+                    break;
+                }
+                if (divisor > 2 && isEven(divisor)) {
+                    divisor++;
+                    continue;
+                }
+                if (dividend % divisor == 0) {
+                    factors.add(divisor);
+                    System.out.println(divisor);
+                    dividend /= divisor;
+                } else {
+                    if (divisor == 2) {
+                        divisor++;
+                    } else {
+                        divisor += 2;
+                    }
+                }
+            } finally {
+                lock.unlock();
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.toString());
+                continue;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RunnableCancellableInterruptiblePrimeFactorizer gen = new RunnableCancellableInterruptiblePrimeFactorizer(256, 2, 8);
+        Thread thread = new Thread(gen);
+        thread.start();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        gen.setDone();
+        thread.interrupt();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n" + gen.getPrimeFactors() + " are the prime factors.");
+
+
+        System.out.print("Factors of 122 are	:\t\t");
+        gen.getPrimeFactors().forEach((Long l) -> System.out.print(l + ", "));
+
+        RunnableCancellableInterruptiblePrimeFactorizer gen1 = new
+                RunnableCancellableInterruptiblePrimeFactorizer(336, 3, 12);
+        RunnableCancellableInterruptiblePrimeFactorizer gen2 = new
+                RunnableCancellableInterruptiblePrimeFactorizer(24, 2, 20);
+
+        Thread thread1 = new Thread(gen1);
+        Thread thread2 = new Thread(gen2);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        gen1.setDone();
+        gen2.setDone();
+        thread1.interrupt();
+        thread2.interrupt();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print("Factors of 336 are	:\t\t");
+        gen1.getPrimeFactors().forEach((Long l) -> System.out.print(l + ", "));
+        System.out.println();
+
+        System.out.print("Factors of 24 are	:\t\t");
+        gen2.getPrimeFactors().forEach((Long l) -> System.out.print(l + ", "));
+    }
+
+}
