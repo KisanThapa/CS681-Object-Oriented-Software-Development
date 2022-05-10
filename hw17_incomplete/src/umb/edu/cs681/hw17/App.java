e5c2acc4dcd814d27793ca686b1ba902f2b0f269
Kisan Thapa <kisanthapa33@gmail.com>
2022-05-10 10:34:49 -0400

    Minor fixes

---
+++ hw17_incomplete/src/umb/edu/cs681/hw17/App.java
@@ -0,0 +1,7 @@
+package umb.edu.cs681.hw17;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ hw18_incomplete/src/edu/umb/cs681/hw18/App.java
@@ -0,0 +1,7 @@
+package edu.umb.cs681.hw18;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
